bea176199599b6d8f1a4f68f0bec35e1536f770e
John Hamm <johndhamm1@gmail.com>
2016-07-18 22:19:39 -0500

    initial commit

---
+++ quiz.js
@@ -0,0 +1,26 @@
+// Code/Tools Requirements
+
+// 1. Use ES6 language features wherever you can. At a minimum, you should be using let, const, fat arrows, property shorthand, method properties, and string templates.
+// 2. Have a Gulp task running at all times to validate your JavaScript. We will be validating your project and we should see 0 errors.
+// 3. You must have a test suite that validates the core logic of the application.
+// 4. You must use jQuery for interacting with the DOM.
+
+// Base Logical Requirements
+
+// You'll be building robots to battle each other.
+
+// 1. A base Robot function.
+// 2. Define three robot type functions (e.g. Drone, Bipedal, ATV).
+// 3. Define at least 2 specific robot model functions for each type.
+// 4. Give each robot model a different range of health. For example, one model can have health range of 50-80, and another one will have a range of 60-120. To accomplish this, read about the Math.random() function in JavaScript.
+// 5. Give each robot model a different range of damage they do using the same technique.
+
+// Base Functional Requirements
+
+// 1. When your user interface first loads, provide 2 text inputs to name the two robots that will do battle.
+// 2. You must also provide a select element underneath each text input so that the user can select one of the 6 robot models you defined.
+// 3. Provide a Attack! button that, when clicked, simply applies the damage output of each robot against the other one.
+// 4. Once either robot's health is <0 display a message that the battle is over, and which one won. For example...
+// 5. The Viper Drone defeated the Behemoth ATV with its flamethrower.
+
+
